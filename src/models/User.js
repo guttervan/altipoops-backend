d31cee6217c9fa762b0df50b07b0152a799ce238
Zachary Chaffee <zachary_chaffee@yahoo.com>
2026-07-30 09:26:39 -0600

    Add Sequelize validation to user model

--- src/models/User.js
+++ src/models/User.js
@@ -13,28 +13,53 @@ const User = sequelize.define(
     displayName: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [2, 50],
+      },
     },
 
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
+      validate: {
+        isEmail: true,
+        notEmpty: true,
+      },
+      set(value) {
+        this.setDataValue(
+          "email",
+          typeof value === "string"
+            ? value.trim().toLowerCase()
+            : value
+        );
+      },
     },
 
     passwordHash: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
 
     homeRegion: {
       type: DataTypes.STRING,
       allowNull: true,
+      validate: {
+        len: [0, 100],
+      },
     },
 
     privacySetting: {
       type: DataTypes.STRING,
       allowNull: false,
       defaultValue: "private",
+      validate: {
+        isIn: [["private", "friends", "public"]],
+      },
     },
   },
   {
